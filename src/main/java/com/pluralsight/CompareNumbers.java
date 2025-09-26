d9035cea20f2680029c7c07978ceeb0b57379ea2
Matiyas <Matiyasafesha@gmail.com>
2025-09-25 22:59:45 -0400

    Second Initial commit

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -2,15 +2,22 @@ package com.pluralsight;
 
 public class CompareNumbers {
     public static void main(String[] args) {
-
+      evenandpositive();
     }
-public static int evenandpositive (){
+public static String evenandpositive (){
         int number =  5 ;
-        if (number % 2 == 0);{
+        int ispositive = 5 ;
+        if (number % 2 == 0){
         System.out.println("Print is even"+ number);
         } else {
         System.out.println("print is odd  "+ number);
     }
+        if(ispositive > 0){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+        return (number + " " +ispositive);
 
 }
 }
